6748b6353591adaa43b88685ea536979c5952728
HarveyPeterForFarRankin <harvey.peter.rankin@outlook.com>
2021-12-30 18:05:18 +0000

    added constants file

--- Backend/Middleware/Authentication/index.ts
+++ Backend/Middleware/Authentication/index.ts
@@ -1,5 +1,6 @@
 const { verifyAccessToken } = require("../../jwt");
 const { responseCreator } = require("../../utils");
+const { AUTH_FAIL, HEADER_MISSING } = require("../../constants");
 
 const validateToken = (req, res, next) => {
   const response = responseCreator(res);
@@ -7,13 +8,13 @@ const validateToken = (req, res, next) => {
   //get authorisation header value
   const bearerHeader = req.headers["authorization"];
   if (bearerHeader === undefined) {
-    return response403({ message: "missing header" });
+    return response403({ message: HEADER_MISSING });
   } else {
     //token is of form 'bearer <tokenvalue>' so separate
     const token = bearerHeader.split(" ")[1];
     // returns userObject or false
     verifyAccessToken(token)((err, tokenData) => {
-      if (err) return response403({ message: "failed to authenticate" });
+      if (err) return response403({ message: AUTH_FAIL });
       req.user = tokenData;
       next();
     });
